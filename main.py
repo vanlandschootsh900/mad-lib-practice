c2f7cb39bbf15373a59688717fc2b1e1b55a229e
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2024-09-16 11:16:45 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello!')
+places = input('places:')
+noun1 = input('Noun:')
+noun2 = input('Noun:')
+noun3 = input('Noun:')
+verb = input('Verb:')
+verb1 = input('Verb:')
+verb2 = input('Verb:')
+
+madlibs = f"Jimmy like to go to {places} and his favriet thing to do there is {verb.upper()} and he normly dose {noun1} after
+then he finds {noun2} and dose {verb2.lower()} and then he gose home and dose {verb1} and ther jimmy gose to bed
+in his {noun3}."
+
+
+print(madlibs)
